798e12372328265bc4fb9d092b48a3df9a6530a4
504 <504@DESKTOP-0CK6R6A>
2022-03-16 21:14:44 +0900

    12321321

--- 1/src/Day11/평가문제_1.java
+++ 1/src/Day11/평가문제_1.java
@@ -9,7 +9,8 @@ public class 평가문제_1 { // c s
 		Scanner scanner = new Scanner(System.in);
 		int[][] 학생점수 = new int[100][5];
 		String[] 이름 = new String[100];
-		int[][] 석차 = new int[100][5];
+		int[] 석차 = new int[100];
+		double[] 평균 = new double[100];
 		
 		while(true) {
 			try {
@@ -22,26 +23,16 @@ public class 평가문제_1 { // c s
 				for(int i=0; i<학생점수.length; i++) {
 					if(학생점수[i][0]!=0) {
 						
-						double avg = (double)학생점수[i][4]/3;
-						int k=0;
-						for(int j=0; j<학생점수.length; j++) {
-							if(학생점수[i][4]>학생점수[j][4]) {
-								석차[k][0]=학생점수[i][0];
-								석차[k][1]=학생점수[i][1];
-								석차[k][2]=학생점수[i][2];
-								석차[k][3]=학생점수[i][3];
-								석차[k][4]=학생점수[i][4];
-							}
-							k++;
-						}
+						
+						
 						System.out.printf(" %d\t%s\t%d\t%d\t%d\t%d\t%.2f\t%d \n",
-								학생점수[i][0], 이름[i], 학생점수[i][1], 학생점수[i][2], 학생점수[i][3], 학생점수[i][4], avg,0) ;
+								학생점수[석차[i]][0], 이름[석차[i]], 학생점수[석차[i]][1], 학생점수[석차[i]][2], 학생점수[석차[i]][3], 학생점수[석차[i]][4], 평균[석차[i]], i+1) ;
 					}
 				}
 				System.out.println("메뉴 : 1. 학생점수 등록 2. 학생점수 삭제");
 				int ch = scanner.nextInt();
-				
 				if(ch==1) { // 학생점수 등록
+					
 					System.out.println("번호 : "); int num = scanner.nextInt();
 					System.out.println("이름 : "); String name = scanner.next();
 					System.out.println("국어 : "); int korean = scanner.nextInt();
@@ -57,7 +48,18 @@ public class 평가문제_1 { // c s
 							학생점수[i][3] = math;
 							학생점수[i][4] = 학생점수[i][1]+학생점수[i][2]+학생점수[i][3];
 							
+							평균[i] = (double)학생점수[i][4]/3;
 							break;
+						}
+					}
+					for(int i=0; i<석차.length; i++) {
+						석차[i]=0;
+					}
+					for(int i=0; i<석차.length; i++) {
+						for(int j=0; j<석차.length; j++) {
+							if(학생점수[i][4]<학생점수[j][4]) {
+								석차[i]=석차[i]+1;
+							}
 						}
 					}
 					
@@ -71,16 +73,25 @@ public class 평가문제_1 { // c s
 							학생점수[i][1]=0;
 							학생점수[i][2]=0;
 							학생점수[i][3]=0;
+							석차[i]=0;
+							이름[i]=null;
+							평균[i]=0.0;
 							for(int j=i; j<학생점수.length; j++) {
 								if(j==학생점수.length-1) {
 									학생점수[j][0]=0;
 									학생점수[j][1]=0;
 									학생점수[j][2]=0;
 									학생점수[j][3]=0;
+									석차[j]=0;
+									이름[j]=null;
+									평균[j]=0.0;
 									break;
 								}
 								else {
 									학생점수[j]=학생점수[j+1];
+									석차[j]=석차[j+1];
+									이름[j]=이름[j+1];
+									평균[j]=평균[j+1];
 								}
 							} // for2 end
 						} // if end
